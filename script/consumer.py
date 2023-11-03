4444228bb6d459a76c481fa392162163e7fc34de
CHRIS123540 <735514368@qq.com>
2023-11-03 12:11:22 +0000

    project

--- script/consumer.py
+++ script/consumer.py
@@ -1,14 +1,20 @@
 import os
+import time
 
 fifo_path = '/tmp/my_fifo'
-fifo = os.open(fifo_path, os.O_RDONLY)
+fifo = open(fifo_path, 'r')  # 以文本模式打开管道
 
 numbers_received = []
-for _ in range(10):
-    for _ in range(10):
-        line = os.read(fifo, 16).decode('utf-8').strip()
-        if line:
-            numbers_received.append(int(line))
-    print(numbers_received)
+line = fifo.readline().strip()  # 使用readline()读取一行
+if line:
+    numbers_received.append(int(line))
+start_time = time.time()
+while time.time() - start_time < 10:
+    line = fifo.readline().strip()  # 使用readline()读取一行
+    if line:
+        numbers_received.append(int(line))
 
-os.close(fifo)
+total_data_received = len(numbers_received)
+print(f'Total data received: {total_data_received}')
+
+fifo.close()  # 关闭管道
--- script/producer.cpp
+++ script/producer.cpp
@@ -9,10 +9,12 @@ int main() {
     mkfifo(fifo_path.c_str(), 0666);
 
     std::ofstream fifo(fifo_path, std::ios::out);
-    for (int i = 1; i <= 100; ++i) {
+    for (int i = 1; i <= 100000; ++i) {  // 更新循环次数以匹配10秒的运行时间
         fifo << i << std::endl;
-        sleep(1);
+   
+//        usleep(100);  // 更新这里，以便每秒写入1000次
     }
     fifo.close();
     return 0;
 }
+
